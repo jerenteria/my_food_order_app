ae56e52cbe48648542be0e40be9736a00a934482
jerenteria <renteria.juan96@gmail.com>
2023-10-07 14:02:13 -0700

    created header and about me with some styling

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="header">Welcome to DashHub!</h1>
-      <button className="order-button">Order</button>
+      <section>
+      <h1 className="header">Welcome to DineHub!</h1>
+      <p className="cart">Cart</p>
+      </section>
+      <section>
+        <div className="about"> 
+        <p>Order delicious food from local restaurants! </p>
+
+        <p>These mouth watering platters will not dissapoint!</p>
+
+        </div>
+      </section>
     </div>
   );
 }
